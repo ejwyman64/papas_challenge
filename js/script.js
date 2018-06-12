6e182e48abf54c4ef54b365417e38f30fee50a5e
Emma Wyman <ejpink4@gmail.com>
2018-06-12 14:41:04 -0400

    Added greeting div, changed name on JS file, added information in the About section.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Variables//
+var formResponse = [];
+
+//functions//
+
+//Funtion to add to HTML within <div> marked with the id 'output'//
+function print(message) {
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = message;
+}
+
