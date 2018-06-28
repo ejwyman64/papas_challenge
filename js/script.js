cb91c3c99422c6ed1ca24e1bab2dd5d068ec7824
Emma Wyman <ejpink4@gmail.com>
2018-06-28 18:18:00 -0400

    added some JS and jQuery, made the colors and form more accessible

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-var formBoxes = [];
 
-const stretchBox = document.getElementById('stretching');
-const stretchDiv = document.getElementById('stretchMinutes');
+//Submit alert
+const submitForm = document.querySelector('#submit');
 
-stretchBox.addEventListener('click', () => {
-    if (stretchDiv.style.display == 'block') {
-        stretchDiv.style.display = 'none';
-    } else {
-        stretchDiv.style.display = 'block';
-    }
+submitForm.addEventListener('click', () => {
+    alert("Thank you for submitting today's points!");
 })
 
+//Hide surprise -----
+
+//When 'reveal' is clicked
+$('.greeting #surprise').hide();
+   
+    $('.greeting #reveal').click(function() {
+        //show the 'surprise'
+        $('.greeting #surprise').show();
+
+        //hide the 'reveal'
+        $('.greeting #reveal').hide();
+    });
+
+    $('.greeting #surprise').click(function() {
+        $('.greeting #reveal').show();
+        $('.greeting #surprise').hide();
+    });
